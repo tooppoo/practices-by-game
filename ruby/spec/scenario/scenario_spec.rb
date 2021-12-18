f1ec554f17dbf23fc39c7b2e5a2d2e4eb905ea33
philomagi <tooppoo56@gmail.com>
2021-12-19 00:04:48 +0900

    シナリオ追加

--- ruby/spec/scenario/scenario_spec.rb
+++ ruby/spec/scenario/scenario_spec.rb
@@ -40,7 +40,7 @@ describe 'Janken Scenario' do
 
         expect(result.winners).to match_array [player2]
       end
-      it 'player1 & player3 is looser' do
+      it 'player1 & player3 are looser' do
         result = game.play
 
         expect(result.losers).to match_array [player1, player3]
@@ -48,6 +48,47 @@ describe 'Janken Scenario' do
       it 'not draw game' do
         result = game.play
         
+        expect(result.is_draw?).to eq false
+      end
+    end
+  end
+
+  context 'player1~3 exists, player1 & player2 show only stone, player3 show only scissors' do
+    let(:player1) {
+      Janken::Player.new(
+        name: "Ulike Norman Owen",
+        strategy: Janken::Strategy::Only.stone
+      )
+    }
+    let(:player2) {
+      Janken::Player.new(
+        name: "Una Nancy Owen",
+        strategy: Janken::Strategy::Only.stone
+      )
+    }
+    let(:player3) {
+      Janken::Player.new(
+        name: "Agatha Christie",
+        strategy: Janken::Strategy::Only.scissors
+      )
+    }
+
+    context 'play 3times' do
+      let(:times) { 3 }
+
+      it 'player1 & player2 are winner because most won' do
+        result = game.play
+
+        expect(result.winners).to match_array [player1, player2]
+      end
+      it 'player3 is looser' do
+        result = game.play
+
+        expect(result.losers).to match_array [player3]
+      end
+      it 'not draw game' do
+        result = game.play
+
         expect(result.is_draw?).to eq false
       end
     end
