093ad3508b996ae828d0e722579e02761310c1c8
philomagi <tooppoo56@gmail.com>
2022-03-20 15:40:23 +0900

    simplify

--- haskell/janken-couple/src/JankenCouple/Player.hs
+++ haskell/janken-couple/src/JankenCouple/Player.hs
@@ -18,15 +18,16 @@ player id name strategy = Player id name strategy 0
 battleWith :: Player -> Player -> (Player, Player)
 battleWith p1 p2 = battleWith' p1 p2 (Hand.battleWith p1Hand p2Hand)
   where
-    Player { strategy = p1Strategy } = p1
-    Player { strategy = p2Strategy } = p2
-    p1Hand = select p1Strategy
-    p2Hand = select p2Strategy
+    p1Hand = selectHand' p1
+    p2Hand = selectHand' p2
 
 battleWith' :: Player -> Player -> Hand.Result -> (Player, Player)
 battleWith' p1 p2 Hand.Win = ((notifyWin' p1), p2)
 battleWith' p1 p2 Hand.Lose =  (p1, (notifyWin' p2))
 battleWith' p1 p2 Hand.Draw =  (p1, p2)
+
+selectHand' :: Player -> Hand.Hand
+selectHand' (Player { strategy = str }) = select str
 
 notifyWin' :: Player -> Player
 notifyWin' p = Player (id p) (name p) (strategy p) ((winCount p) + 1)
